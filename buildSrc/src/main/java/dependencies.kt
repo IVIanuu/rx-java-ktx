2804f51d1e666daaef93e92a7aa664e14f8ffea4
IVIanuu <IVIanuu@gmail.com>
2018-09-30 14:18:21 +0200

    Removed local maven stuff

--- buildSrc/src/main/java/dependencies.kt
+++ buildSrc/src/main/java/dependencies.kt
@@ -6,8 +6,7 @@ object Versions {
     const val minSdk = 14
     const val targetSdk = 28
     const val versionCode = 1
-    const val versionName = "SNAPSHOT"
-    const val groupId = "com.ivianuu.rxjavaktx"
+    const val versionName = "0.0.1"
 
     const val androidGradlePlugin = "3.2.0"
     const val androidx = "1.0.0"
